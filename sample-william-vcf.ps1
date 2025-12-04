76c1035f8e5c27260c8f4d0695a0f6bc86e60f5a
emphyrius <147014993+emphyrius@users.noreply.github.com>
2025-12-04 02:30:27 +0000

    commit

--- sample-william-vcf.ps1
+++ sample-william-vcf.ps1
@@ -1,37 +1,37 @@
 # Physical vCenter Server environment
-$VIServer = ""
-$VIUsername = ""
-$VIPassword = ""
+$VIServer = "vcf-vc01.tech-lab-vcf.eng.cdwbuilt.com"
+$VIUsername = "administrator@vsphere.local"
+$VIPassword = "VMw@rE123!VSEeF!"
 
 # General Deployment Configuration
-$VAppLabel = "WilliamLam-VCF9"
+$VAppLabel = "MarkB-Nested-VCF9"
 $VMDatacenter = "Datacenter"
 $VMCluster = "MyCluster"
 $VMNetwork = "MyNetwork"
 $VMDatastore = "MyDatastore"
 $VMNetmask = "255.255.0.0"
-$VMGateway = "172.16.1.53"
-$VMDNS = "172.16.1.3"
-$VMNTP = "172.16.1.53"
-$VMPassword = "VMware1!"
+$VMGateway = "10.90.1.1"
+$VMDNS = "10.90.1.253"
+$VMNTP = "time1.google.com,time2.google.com"
+$VMPassword = "2025vmwareNSX1!!"
 $VMDomain = "vcf.lab"
 $VMSyslog = "172.16.1.250"
-$VMFolder = "wlam"
+$VMFolder = "markb-nested-vcf9"
 
 # Enable Debugging
 $Debug = $false
 
 # Full Path to both the Nested ESXi & VCF Installer OVA
-$NestedESXiApplianceOVA = "/images/Nested_ESXi9.0_Appliance_Template_v1.0.ova"
-$VCFInstallerOVA = "/images/VCF-SDDC-Manager-Appliance-9.0.0.0.24703748.ova"
+$NestedESXiApplianceOVA = "W:\VMware\Nested ESXi\Nested_ESXi9.0_Appliance_Template_v1.0.ova"
+$VCFInstallerOVA = "W:\VMware\VCF 9\VCF-SDDC-Manager-Appliance-9.0.1.0.24962180.ova"
 
 # VCF Version
-$VCFInstallerProductVersion = "9.0.0.0"
+$VCFInstallerProductVersion = "9.0.1.0"
 $VCFInstallerProductSKU = "VCF"
 
 # VCF Software Depot Configuration
-$VCFInstallerSoftwareDepot = "offline" #online or offline
-$VCFInstallerDepotToken = ""
+$VCFInstallerSoftwareDepot = "online" #online or offline
+$VCFInstallerDepotToken = "M6F92JtVhIQbqIfacKFGZRN57cFLeInA"
 
 # Offline Depot Configurations (optional)
 $VCFInstallerDepotUsername = "vcf"
@@ -41,39 +41,39 @@ $VCFInstallerDepotPort = 8888
 $VCFInstallerDepotHttps = $false
 
 # VCF Fleet Deployment Configuration
-$DeploymentInstanceName = "William VCF 9 Instance"
+$DeploymentInstanceName = "nest-vcf9"
 $DeploymentId = "vcf-m01"
 $CEIPEnabled = $true
 $FIPSEnabled = $true
 
 # VCF Installer Configurations
 $VCFInstallerVMName = "inst01"
-$VCFInstallerFQDN = "inst01.vcf.lcm"
-$VCFInstallerIP = "172.16.30.10"
+$VCFInstallerFQDN = "inst01.vcf.lab"
+$VCFInstallerIP = "10.90.1.217"
 $VCFInstallerAdminUsername = "admin@local"
-$VCFInstallerAdminPassword = "VMware1!VMware1!"
-$VCFInstallerRootPassword = "VMware1!VMware1!"
+$VCFInstallerAdminPassword = "2025vmwareNSX1!!"
+$VCFInstallerRootPassword = "2025vmwareNSX1!!"
 
 # SDDC Manager Configuration
 $SddcManagerHostname = "sddcm01"
-$SddcManagerIP = "172.16.30.11"
-$SddcManagerRootPassword = "VMware1!VMware1!"
-$SddcManagerVcfPassword = "VMware1!VMware1!"
-$SddcManagerSSHPassword = "VMware1!VMware1!"
-$SddcManagerLocalPassword = "VMware1!VMware1!"
+$SddcManagerIP = "10.90.1.210"
+$SddcManagerRootPassword = "2025vmwareNSX1!!"
+$SddcManagerVcfPassword = "2025vmwareNSX1!!"
+$SddcManagerSSHPassword = "2025vmwareNSX1!!"
+$SddcManagerLocalPassword = "2025vmwareNSX1!!"
 
 # Nested ESXi VMs for Management Domain
 $NestedESXiHostnameToIPsForManagementDomain = @{
-    "esx01"   = "172.16.30.1"
-    "esx02"   = "172.16.30.2"
-    "esx03"   = "172.16.30.3"
+    "esxi01.vcf.lab" = "10.90.1.201"
+    "esxi02.vcf.lab" = "10.90.1.202"
+    "esxi03.vcf.lab" = "10.90.1.203"
 }
 
 # Nested ESXi VMs for Workload Domain
 $NestedESXiHostnameToIPsForWorkloadDomain = @{
-    "esx04"   = "172.16.30.4"
-    "esx05"   = "172.16.30.5"
-    "esx06"   = "172.16.30.6"
+    "esxi04.vcf.lab" = "10.90.1.204"
+    "esxi05.vcf.lab" = "10.90.1.205"
+    "esxi06.vcf.lab" = "10.90.1.206"
 }
 
 # Nested ESXi VM Resources for Management Domain
@@ -91,16 +91,16 @@ $NestedESXiWLDCapacityvDisk = "250" #GB
 $NestedESXiWLDBootDisk = "64" #GB
 
 # ESXi Network Configuration
-$NestedESXiManagementNetworkCidr = "172.16.0.0/16" # should match $VMNetwork configuration
+$NestedESXiManagementNetworkCidr = "10.90.1.0/24" # should match $VMNetwork configuration
 $NestedESXivMotionNetworkCidr = "10.1.32.0/24"
 $NestedESXivSANNetworkCidr = "10.1.33.0/24"
 $NestedESXiNSXTepNetworkCidr = "10.1.34.0/24"
 
 # vCenter Configuration
 $VCSAName = "vc01"
-$VCSAIP = "172.16.30.13"
-$VCSARootPassword = "VMware1!VMware1!"
-$VCSASSOPassword = "VMware1!VMware1!"
+$VCSAIP = "10.90.1.211"
+$VCSARootPassword = "2025vmwareNSX1!!"
+$VCSASSOPassword = "2025vmwareNSX1!!"
 $VCSASize = "small"
 $VCSAEnableVCLM = $true
 $VCSADatacenterName = "vcf-mgmt-dc"
@@ -114,35 +114,35 @@ $VSANDatastoreName = "vsanDatastore"
 
 # VCF Operations Configuration
 $VCFOperationsSize = "small"
-$VCFOperationsHostname = "vcf01"
-$VCFOperationsIP = "172.16.30.12"
-$VCFOperationsRootPassword = "VMware1!VMware1!"
-$VCFOperationsAdminPassword = "VMware1!VMware1!"
+$VCFOperationsHostname = "vcf01.vcf.lab"
+$VCFOperationsIP = "10.90.1.219"
+$VCFOperationsRootPassword = "2025vmwareNSX1!!"
+$VCFOperationsAdminPassword = "2025vmwareNSX1!!"
 
 # NSX Configuration
 $NSXManagerSize = "medium"
-$NSXManagerVIPHostname = "nsx01"
-$NSXManagerVIPIP = "172.16.30.14"
-$NSXManagerNodeHostname = "nsx01a"
-$NSXRootPassword = "VMware1!VMware1!"
-$NSXAdminPassword = "VMware1!VMware1!"
-$NSXAuditPassword = "VMware1!VMware1!"
+$NSXManagerVIPHostname = "nsxvip"
+$NSXManagerVIPIP = "10.90.1.212"
+$NSXManagerNodeHostname = "nsxmg01"
+$NSXRootPassword = "2025vmwareNSX1!!"
+$NSXAdminPassword = "2025vmwareNSX1!!"
+$NSXAuditPassword = "2025vmwareNSX1!!"
 
 # VCF Operations Collector
 $VCFOperationsCollectorSize = "small"
 $VCFOperationsCollectorHostname = "opsproxy01"
-$VCFOperationsCollectorRootPassword = "VMware1!VMware1!"
+$VCFOperationsCollectorRootPassword = "2025vmwareNSX1!!"
 
 # VCF Operations Fleet Manager
 $VCFOperationsFleetManagerHostname = "opsfm01"
-$VCFOperationsFleetManagerAdminPassword = "VMware1!VMware1!"
-$VCFOperationsFleetManagerRootPassword = "VMware1!VMware1!"
+$VCFOperationsFleetManagerAdminPassword = "2025vmwareNSX1!!"
+$VCFOperationsFleetManagerRootPassword = "2025vmwareNSX1!!"
 
 # VCF Automation
 $VCFAutomationHostname = "auto01"
-$VCFAutomationAdminPassword = "VMware1!VMware1!"
-$VCFAutomationIPPool = @("172.16.30.23","172.16.30.24")
-$VCFAutomationNodePrefix = "vcf-lamw-auto"
+$VCFAutomationAdminPassword = "2025vmwareNSX1!!"
+$VCFAutomationIPPool = @("10.90.1.241","10.90.1.242")
+$VCFAutomationNodePrefix = "vcf-markb-auto"
 $VCFAutomationClusterCIDR = "198.18.0.0/15"
 
 # VCF Workload Domain Configurations
